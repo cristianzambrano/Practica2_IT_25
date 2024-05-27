41bcbd4c442430061e1b87c9fd3ba7f0c426c29b
JOELITO07 <joel_jc007@hotmail.com>
2024-05-27 12:14:31 -0500

    Adding code of Login Practice

--- app/src/main/java/com/example/practica2_it_2024/MainActivity.java
+++ app/src/main/java/com/example/practica2_it_2024/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.practica2_it_2024;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +26,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void clickBotonIngresar(View view){
+
+        TextInputLayout tilUsario = findViewById(R.id.tilUsuario);
+        TextInputEditText txtCedula = findViewById(R.id.txtCedula);
+        String cedula = txtCedula.getText().toString();
+        if (cedula.equals("") || cedula.length()!=10){
+            tilUsario.setError("Ingrese una cédula válida");
+        }else
+            tilUsario.setError("");
+    }
+    public void clickBotonCancelar(View view){
+
     }
 }
